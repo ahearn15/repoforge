230a1d71557ec24c07e8835d309e99a85fbd30b3
ahearn15 <ahearn15@gmail.com>
2025-02-16 21:02:59 -0500

    ver 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="repoforge",       # Package name on PyPI
-    version="0.1.0",
+    version="0.1.1",
     author="Adam Hearn",
     author_email="ahearn15@gmail.com",
     description="Generate a formatted LLM prompt from a repository directory.",
